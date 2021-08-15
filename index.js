e6f3c1772067d08e6bc83c31fd81b461063762fb
Sam Robbins <samrobbinsgb@gmail.com>
2021-08-15 19:14:42 +0100

    Fetch national rail data as JSON

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import c from "ansi-colors"
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+import c from "ansi-colors";
+const useStaffVersion = false;
+const LiveDepartureBoardService = require("ldbs-json");
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response(c.blue('!Hello worker!\n'))
+  const options = {
+    crs: "DHM",
+  };
+  const api = new LiveDepartureBoardService(nationalrail, useStaffVersion);
+  const resp = await api.call("GetDepBoardWithDetails", options);
+  return new Response(JSON.stringify(resp), {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
 }
-
