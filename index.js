242fecac233bfbeb65e024865eed45f8eace8acd
Sam Robbins <samrobbinsgb@gmail.com>
2021-08-17 19:09:50 +0100

    ✨ Basic departure board for fixed location

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import c from "ansi-colors";
+import { table } from "table";
 const useStaffVersion = false;
 const LiveDepartureBoardService = require("ldbs-json");
 addEventListener("fetch", (event) => {
@@ -14,9 +15,29 @@ async function handleRequest(request) {
   };
   const api = new LiveDepartureBoardService(nationalrail, useStaffVersion);
   const resp = await api.call("GetDepBoardWithDetails", options);
-  return new Response(JSON.stringify(resp), {
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
-  });
+  const data = resp.GetStationBoardResult.trainServices.service.map((item) => [
+    c.yellowBright(item.std),
+    c.yellowBright(item.destination.location.crs),
+    c.yellowBright(item.platform || ""),
+    c.yellowBright(item.etd),
+  ]);
+  data.unshift(["Time", "Destination", "Plat", "Expected"]);
+  // const data = [
+  //   ["0A", "0B", "0C"],
+  //   ["1A", "1B", "1C"],
+  //   ["2A", "2B", "2C"],
+  // ];
+  // return new Response(
+  //   c.blue(
+  //     `${resp.GetStationBoardResult.trainServices.service[0].destination.location.crs}\n`
+  //   )
+  // );
+  return new Response(
+    table(data, {
+      header: {
+        alignment: "center",
+        content: resp.GetStationBoardResult.locationName,
+      },
+    })
+  );
 }
