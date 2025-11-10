ee183e0ed8e0bb87bdf8c8bb3fe90c6e9613fc0c
Sam Robbins <samrobbinsgb@gmail.com>
2025-11-10 19:48:53 +0000

    Upgrade vite

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [reactRefresh()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  plugins: [react(), tailwind()],
 });
