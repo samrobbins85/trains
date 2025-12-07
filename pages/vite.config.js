68fbf677560fe3e6b8c7cc8cfd9105cac80ee9e9
Sam Robbins <samrobbinsgb@gmail.com>
2025-12-07 19:28:40 +0000

    Replace old page with hono

--- pages/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite";
-export default defineConfig({
-  plugins: [react(), tailwind()],
-});
